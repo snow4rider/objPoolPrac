5b0a174205f28997d002d4f2a243004d6146624b
Matthew Mckenney <matthew.t.mckenney@gmail.com>
2024-07-07 18:01:33 -0600

    working

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include "obj.cpp"
+
 
 int main ()
 {
-  std::cout << "hello World" << std::endl;
+	std::cout << "hello World" << std::endl;
+	
+	Obj number;
 
-  return 0;
+	number.setData(45);
+
+	std::cout << number.getData() << std::endl;
+	
+	return 0;
 }
---
+++ src/obj.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+class Obj
+{
+	private:
+		int data;
+	public:
+		void setData(int info)
+		{
+			data = info;
+		}
+
+		int getData()
+		{
+			return data;
+		}
+};
+
